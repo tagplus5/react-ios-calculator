50849749e290395056df496274ceeb2a4d752561
tagplus5 <tagplus5@gmail.com>
2021-07-18 17:38:05 +0300

    rm Display warning

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,7 +1,11 @@
-const Display = ({ value }) => (
-   <div id="display" className="flex">
-      <input type="text" tabIndex="-1" value={value} />
-   </div>
-);
+const Display = ({ value }) => {
+   const handleChange = () => { };
+
+   return (
+      <div id="display" className="flex">
+         <input type="text" tabIndex="-1" value={value} onChange={handleChange} />
+      </div>
+   );
+};
 
 export default Display;
